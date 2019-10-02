332550b320f0aa87cd18c4450211f3496fc20d02
Simon Coulton <simon.coulton@gmail.com>
2019-10-02 10:56:49 +1000

    chore: poetry fix

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,7 +1,6 @@
 export ZSH="${ZDOTDIR:-$HOME}/.zsh"
 export ZPREZTODIR="${ZDOTDIR:-$HOME}/.zprezto"
 export ZSH_SOURCES="$ZSH/sources"
-export PATH=$PATH:$HOME/.poetry/bin
 
 source $ZPREZTODIR/init.zsh
 for f in $(find "$ZSH_SOURCES/" -maxdepth 1 -type f -name \*.zsh) ; do
--- zsh/sources/aliases.zsh
+++ zsh/sources/aliases.zsh
@@ -8,6 +8,8 @@ alias dks="docker kill $(docker ps -q)"
 alias dkrmc="docker rm $(docker ps -a -q)"
 alias de="docker exec -it $1 $2"
 alias drmi="docker images | grep none | awk '{ print $3; }' | xargs docker rmi"
+# To get around https://github.com/sdispater/poetry/issues/536
+alias poetry="python3 $HOME/.poetry/bin/poetry"
 
 pyclean () {
     find . -type f -name "*.py[co]" -delete
