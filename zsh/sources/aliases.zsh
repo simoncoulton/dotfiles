b5048ae9a8f86f31efd64512589a4364a3cbfc61
Simon Coulton <simon.coulton@gmail.com>
2020-11-20 15:26:25 +1100

    feat: additional aliases, open vscode workspaces, docker etc

--- zsh/sources/aliases.zsh
+++ zsh/sources/aliases.zsh
@@ -4,10 +4,17 @@ alias vimrc="vim ~/.vimrc"
 alias vea="source .venv/bin/activate"
 alias ved="deactivate"
 alias it="itermocil"
+
 alias dks="docker kill $(docker ps -q)"
 alias dkrmc="docker rm $(docker ps -a -q)"
 alias de="docker exec -it $1 $2"
 alias drmi="docker images | grep none | awk '{ print $3; }' | xargs docker rmi"
+alias dsp="docker system prune -a"
+alias dvp="docker volume prune"
+alias dps="docker ps"
+
+alias codep="find . -iname *\.code-workspace -exec code {} \;"
+
 # To get around https://github.com/sdispater/poetry/issues/536
 alias poetry="python3 $HOME/.poetry/bin/poetry"
 alias fh="fuck --hard"
