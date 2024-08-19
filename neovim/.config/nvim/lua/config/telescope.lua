8745eb5a8325d4ed5be67d6eae9284a2bccaedc1
Simon Coulton <simon.coulton@gmail.com>
2024-08-20 01:15:53 +1000

    feat: telescope additions

--- neovim/.config/nvim/lua/config/telescope.lua
+++
@@ -1,6 +0,0 @@
-local builtin = require("telescope.builtin")
-
--- vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "[C]ommand [P]alette" })
--- vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind [G]rep" })
--- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "[F]ind [B]uffers"})
--- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "[F]ind [H]elp" })
--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,63 +1,81 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-ui-select.nvim" },
-    keys = {
-      {
-        "<leader>sg",
-        function()
-          require("telescope.builtin").git_files()
-        end,
-        desc = "[S]earch [G]it Files",
-      },
-      {
-        "<leader>fg",
-        function()
-          require("telescope.builtin").live_grep()
-        end,
-        desc = "[F]ind [G]rep",
-      },
-      {
-        "<C-p>",
-        function()
-          require("telescope.builtin").find_files()
-        end,
-        desc = "[C]command [P]alette",
-      },
-      {
-        "<leader>fb",
-        function()
-          require("telescope.builtin").buffers()
-        end,
-        desc = "[F]ind [B]uffers",
-      },
-      {
-        "<leader>fh",
-        function()
-          require("telescope.builtin").help_tags()
-        end,
-        desc = "[F]ind [H]elp",
-      },
-      {
-        "<leader>o",
-        function()
-          require("telescope.builtin").buffers()
-        end,
-        desc = "Buffers",
-      },
-    },
-    config = function()
-      local telescope = require("telescope")
-
-      telescope.setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-ui-select.nvim" },
+		keys = {
+			{
+				"<leader>sg",
+				function()
+					require("telescope.builtin").git_files()
+				end,
+				desc = "[S]earch [G]it Files",
+			},
+			{
+				"<leader>fg",
+				function()
+					require("telescope.builtin").live_grep()
+				end,
+				desc = "[F]ind [G]rep",
+			},
+			{
+				"<C-p>",
+				function()
+					require("telescope.builtin").find_files()
+				end,
+				desc = "[C]command [P]alette",
+			},
+			{
+				"<leader>fb",
+				function()
+					require("telescope.builtin").buffers()
+				end,
+				desc = "[F]ind [B]uffers",
+			},
+			{
+				"<leader>fh",
+				function()
+					require("telescope.builtin").help_tags()
+				end,
+				desc = "[F]ind [H]elp",
+			},
+			{
+				"<leader>o",
+				function()
+					require("telescope.builtin").buffers()
+				end,
+				desc = "Buffers",
+			},
+		},
+		config = function()
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
+			telescope.setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+				-- buffers = {
+				-- 	mappings = {
+				-- 		i = {
+				-- 			["<C-b>"] = actions.delete_buffer,
+				-- 		},
+				-- 		n = {
+				-- 			["<C-b>"] = actions.delete_buffer,
+				-- 		},
+				-- 	},
+				-- 	previewer = false,
+				-- 	initial_mode = "normal",
+				-- 	layout_config = {
+				-- 		height = 0.4,
+				-- 		width = 0.6,
+				-- 		prompt_position = "top",
+				-- 		preview_cutoff = 120,
+				-- 	},
+				-- },
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
