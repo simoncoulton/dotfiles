287d9c27798e2d65617b05f25f84553811a963e4
Simon Coulton <simon.coulton@gmail.com>
2024-08-19 21:33:34 +1000

    chore: fonts in nvim

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -1,13 +1,22 @@
 local opts = vim.opt
 
 opts.expandtab = true
+
 opts.tabstop = 2
 opts.softtabstop = 2
 opts.shiftwidth = 2
+
 opts.number = true
 opts.relativenumber = true
 opts.signcolumn = "number"
+
 opts.clipboard = "unnamed"
+
 opts.termguicolors = true
-opts.guifont = "FiraCode Nerd Font"
+-- opts.guifont = "Hack Nerd Font"
+-- opts.guifont = "FiraCode Nerd Font"
+opts.guifont = "MesloLGL Nerd Font"
+
 opts.cursorline = true
+
+opts.showmode = false
