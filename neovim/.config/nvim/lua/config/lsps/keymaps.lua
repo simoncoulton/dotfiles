7b043ea1f16ff4e07805b7cdbb197fe12a011890
Simon Coulton <simon.coulton@gmail.com>
2024-08-19 18:26:38 +1000

    feat: dashboard

--- neovim/.config/nvim/lua/config/lsps/keymaps.lua
+++ neovim/.config/nvim/lua/config/lsps/keymaps.lua
@@ -1,8 +1,8 @@
 local opts = { buffer = bufnr, noremap = true, silent = true }
 
-vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+--vim.keymap.set("n", "<leader>d", vim.lsp.diagnostic.show_line_diagnostics, opts)
---
+++ neovim/.config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,6 @@
+return {
+	"goolord/alpha-nvim",
+	config = function()
+		require("config.alpha")
+	end,
+}
